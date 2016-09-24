da530607ba50169421a5ca8d1245d9315ca4846b
Vaibhav Bhandari <vaibhavb@vitraag.com>
2016-09-24 11:44:27 -0700

    updates Day #2

--- dataviz/train_vel_viz.py
+++ dataviz/train_vel_viz.py
@@ -1,5 +1,6 @@
 # Simple DataViz for the train routes
 import sys
+from mpl_toolkits.basemap import Basemap, cm
 import matplotlib.pyplot as plt
 import string
 
@@ -9,8 +10,6 @@ lon=[] #longitudes
 lat=[] #latitudes
 redlon = []
 redlat = []
-lon_bad=[]
-lat_bad=[]
 filename_ext = sys.argv[1]
 filename = filename_ext.split('.')[0]
 fi=open(filename_ext,'r')
@@ -30,8 +29,8 @@ for line in fi:
         try:
             fields=string.split(line,",")
             x,y=fields[2:4]
-            vel=fields[6]
-            if vel > 0:
+            vel=float(fields[6])
+            if vel>0:
                 lon.append(float(y))
                 lat.append(float(x))
             else:
@@ -42,7 +41,7 @@ for line in fi:
     linenum+=1
 fi.close()
 
-plt.plot(lon, lat, color = "green", lw = 0.8, alpha = 0.8)
 plt.plot(redlon, redlat, color = "red", lw = 0.8, alpha = 0.8)
-filename = "%s.%s" % (filename,'png')
-plt.savefig(filename, facecolor = fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=300)
+plt.show()
+#filename = "%s.%s" % (filename,'png')
+#plt.savefig(filename, facecolor = fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=300)
