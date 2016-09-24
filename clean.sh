e6a04f4b51d62848fd8b0c3557ae6869bff412a1
Mark Weiss <markweiss@Marks-MacBook-Pro.local>
2016-09-23 23:17:09 -0700

    bug fix

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 train(){
     for i in $(ls $1); do
-    awk  'FNR > 1 -F"," {print $1 $14 $15 $16 $18 $20 $21 $25}' $i > 'temp.csv'
+    awk  -F"," 'FNR > 1 {print $1","$14","$15","$16","$18","$20","$21","$25}' $i > 'temp.csv'
     cat 'temp.csv' >> $2
     done
+    rm 'temp.csv'
     }
